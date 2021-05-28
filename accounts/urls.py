f2f296b3939140a7ea3718baaabe7c6ee4fffdd0
LeandroSimo <leo.bt.12@hotmail.com>
2021-05-28 03:32:30 -0300

    Custom Form Cadastro

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from accounts.views import register
 from django.urls import path,include 
 from django.conf.urls import url
 from django.views.generic import TemplateView
@@ -8,6 +9,8 @@ app_name = 'accounts'
 urlpatterns = [
 
     path('entrar/', TemplateView.as_view(template_name="login.html"),name='login'),
+    path('cadastre-se/', register,name='register'),
+    
     #path('entrar/', include('django.contrib.auth.views.login', {'template_name':'login.html'}, name='login')),
     #path(r'^entrar/$', login, name='login'),
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
+
+
+def register(request):
+    template_name = 'register.html'
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+         form = UserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, template_name, context)
+
+
+
 
