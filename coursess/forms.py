c4b65c9501df62cd084509f39772ce2b3a3a8eef
LeandroSimo <leo.bt.12@hotmail.com>
2021-05-28 00:26:01 -0300

    View de login

--- coursess/forms.py
+++ coursess/forms.py
@@ -1,22 +1,27 @@
 from django import forms
-from django.core.mail import message, send_mail
+from django.core.mail import send_mail
 from django.conf import settings
+from core.mail import send_mail_template
+
+
 
 class ContactCourse(forms.Form):
     
     name = forms.CharField(label='None', max_length=100)
     email = forms.EmailField(label='E-mail')
-    messade = forms.CharField(
+    message = forms.CharField(
         label='Mensagem/Dúvida', widget=forms.Textarea
     )
     
     def send_email(self, course):
-        subject = '[%$] Contato' % course
-        message = 'Nome: %(name)s; E-mail: %(email)s; %(message)s'
+        subject = '[%s]Contato' % course
         context = {
             'name': self.cleaned_data['name'],
             'email': self.cleaned_data['email'],
             'message': self.cleaned_data['message']
         }
-        message = message % context
-        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [settings.CONTACT_EMAIL])
+        template_name = 'contact_email.html'
+        send_mail_template(
+            subject, template_name, context,
+             [settings.CONTACT_EMAIL]
+        )
