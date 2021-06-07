dba4465eed14dfabcaeee90beda038cc2545a373
LeandroSimo <leo.bt.12@hotmail.com>
2021-06-07 14:52:24 -0300

    Testando Forms e E-mail

--- core/tests.py
+++ core/tests.py
@@ -1,17 +1,18 @@
 from django.core import mail
 from django.test import TestCase
 from django.test.client import Client
-from django.shortcuts import resolve_url as r
-
+from django.urls import reverse
 
 
 class HomeViewTest(TestCase):
 
-    def test_home_status_code(self):       
-        self.response = self.client.get(r('core:home'))
-        self.assertEqual(self.response.status_code, 200)
+    def test_home_status_code(self):
+        client = Client()
+        response = client.get(reverse('core:home'))
+        self.assertEqual(response.status_code, 200)
 
     def test_home_template_used(self):
-        self.response = self.client.get(r('core:home'))
-        self.assertTemplateUsed(self.response, 'home.html')
-        self.assertTemplateUsed(self.response, 'base.html')
+        client = Client()
+        response = client.get(reverse('core:home'))
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'base.html')
