407fa50391a8321d753646a6bb233faf4e04fe37
LeandroSimo <leo.bt.12@hotmail.com>
2021-06-07 14:14:21 -0300

    Testando Views

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,17 @@
+from django.core import mail
 from django.test import TestCase
+from django.test.client import Client
+from django.shortcuts import resolve_url as r
 
-# Create your tests here.
+
+
+class HomeViewTest(TestCase):
+
+    def test_home_status_code(self):       
+        self.response = self.client.get(r('core:home'))
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_home_template_used(self):
+        self.response = self.client.get(r('core:home'))
+        self.assertTemplateUsed(self.response, 'home.html')
+        self.assertTemplateUsed(self.response, 'base.html')
