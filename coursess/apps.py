cb374cc69a0abf83826aefbed9a39dc599a831e7
LeandroSimo <leo.bt.12@hotmail.com>
2021-06-09 10:28:53 -0300

    Correção na app

--- coursess/apps.py
+++ coursess/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class CoursessConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'coursess'
+    label = 'Cursos'
+    verbose_name = ('Cursos')
